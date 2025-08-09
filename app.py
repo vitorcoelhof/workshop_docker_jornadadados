3c71669297d84592e61a6b81a81b55092b3fd9da
vitorcoelhof <vitorcoelhof@gmail.com>
2025-08-09 17:45:04 -0300

    WORKSHOP

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def hello_word():
+    return "Hello, World!"
+
+def main():
+    st.title("Hello World App")
+    st.write(hello_word())
+
+if __name__ == "__main__":
+    main()
+# projeto_crud/README.md
